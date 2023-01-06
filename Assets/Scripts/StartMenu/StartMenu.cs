4159cd04fe7c753449d4bac5fd2a8de3e0fbeb0d
I12 <i.tupikov12@gmail.com>
2023-01-07 04:30:43 +0500

    Add initial scene

---
+++ Assets/Scripts/StartMenu/StartMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace OrderElimination.Start
+{
+    public class StartMenu : MonoBehaviour
+    {
+        public void StartButtonClicked()
+        {
+            SceneManager.LoadScene("StrategyMap");
+        }
+
+        public void SettingsButtonClicked()
+        {
+            Debug.Log("Settings Button is clicked");
+        }
+
+        public void ExitButtonClicked()
+        {
+            Application.Quit();
+        }
+    }
+}
