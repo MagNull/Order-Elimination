ca5f2e3994d7c377e57440bf721fcf3380e5c0f5
I12 <i.tupikov12@gmail.com>
2023-01-08 04:08:33 +0500

    Processing switch button's color in Saves

--- Assets/Scripts/StartMenu/Saves.cs
+++ Assets/Scripts/StartMenu/Saves.cs
@@ -1,11 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace OrderElimination.Start
 {
     public class Saves : MonoBehaviour
     {
-        public static event Action<bool> ExitSavesWindow; 
+        public static event Action<bool> ExitSavesWindow;
+        private List<GameObject> _saves;
+        private Image _selectedSavesImages;
+        private Color _activeImageColor = Color.white;
+        private Color _defaultImageColor = Color.gray;
+        
+        private void Awake()
+        {
+            _saves = new List<GameObject>
+            {
+                GameObject.Find("FirstSave"),
+                GameObject.Find("SecondSave"),
+                GameObject.Find("ThirdSave")
+            };
+            
+            FirstSaveClicked();
+        }
+
+        public void FirstSaveClicked()
+        {
+            _selectedSavesImages = _saves[0].GetComponent<Image>();
+            SwitchColorButtons();
+        }
+        
+        public void SecondSaveClicked()
+        {
+            _selectedSavesImages =_saves[1].GetComponent<Image>();
+            SwitchColorButtons();
+        }
+
+        public void ThirdSaveClicked()
+        {
+            _selectedSavesImages = _saves[2].GetComponent<Image>();
+            SwitchColorButtons();
+        }
+
+        private void SwitchColorButtons()
+        {
+            foreach (var image in _saves.Select(save => save.GetComponent<Image>()))
+            {
+                image.color = _selectedSavesImages == image
+                    ? _activeImageColor
+                    : _defaultImageColor;
+            }
+        }
+
+        private void SaveButtonClicked()
+        {
+            
+        }
+        
 
         public void ExitClicked()
         {
