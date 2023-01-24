3ad2ad0b1f9f7f642ceddf603b455b7559d12f80
I12 <i.tupikov12@gmail.com>
2023-01-24 17:51:30 +0500

    Add upgrade to character

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -8,8 +8,10 @@ namespace OrderElimination
     [CreateAssetMenu(fileName = "CharacterInfo", menuName = "Character")]
     public class Character : ScriptableObject, IBattleCharacterInfo
     {
+        private BattleStats _baseStats;
         [SerializeField]
         private BattleStats _battleStats;
+        [SerializeField]
         private StrategyStats _strategyStats;
         [SerializeField]
         private string _name;
@@ -33,13 +35,29 @@ namespace OrderElimination
         public AbilityInfo[] GetPassiveAbilityInfos() => _passiveAbilities;
 
         public StrategyStats GetStrategyStats() => _strategyStats;
+
         public void RaiseExperience(float experience)
         {
             throw new System.NotImplementedException();
         }
 
+        public void Upgrade()
+        {
+            var battleStats = new BattleStats(_battleStats) 
+            {
+                UnmodifiedHealth = _strategyStats.HealthGrowth + _battleStats.UnmodifiedHealth,
+                UnmodifiedArmor = _strategyStats.ArmorGrowth + _battleStats.UnmodifiedArmor,
+                UnmodifiedAccuracy = _strategyStats.AccuracyGrowth + _battleStats.UnmodifiedAccuracy,
+                UnmodifiedEvasion = _strategyStats.EvasionGrowth + _battleStats.UnmodifiedEvasion,
+                UnmodifiedAttack = _strategyStats.AttackGrowth + _battleStats.UnmodifiedAttack
+            };
+            
+            _battleStats = battleStats;
+        }
+
         private void OnValidate()
         {
+            _baseStats = _battleStats;
             _battleStats.UnmodifiedHealth = _battleStats.Health;
             _battleStats.UnmodifiedArmor = _battleStats.Armor;
             _battleStats.UnmodifiedAttack = _battleStats.Attack;
--- Assets/Scripts/Character/StrategyStats.cs
+++ Assets/Scripts/Character/StrategyStats.cs
@@ -5,6 +5,11 @@ namespace OrderElimination
     [Serializable]
     public struct StrategyStats
     {
-        
+        public int CostOfUpgrade;
+        public readonly int HealthGrowth;
+        public readonly int ArmorGrowth;
+        public readonly int AttackGrowth;
+        public readonly int AccuracyGrowth;
+        public readonly int EvasionGrowth;
     }
 }
