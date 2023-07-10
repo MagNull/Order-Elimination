a09ac650b2a7e7d27d9a5eb38d2e75da5a5f9f72
Tupi8mpl <i.tupikov12@gmail.com>
2023-07-11 04:33:02 +0500

    Added the ability to choose identical characters

--- Assets/Scripts/RoguelikeMap/UI/Characters/DraggableObject.cs
+++ Assets/Scripts/RoguelikeMap/UI/Characters/DraggableObject.cs
@@ -1,4 +1,7 @@
+using System;
+using OrderElimination;
 using Sirenix.OdinInspector;
+using StartSessionMenu.ChooseCharacter.CharacterCard;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -13,47 +16,78 @@ namespace RoguelikeMap.UI.Characters
         private Image _image;
         [SerializeField, Range(0, 1), ShowIf("_isTransparency")] 
         private float _aplha;
+        [field: SerializeField]
+        public bool IsCreateCopy { get; private set; }
+        
         private Transform _initialParent;
         private Transform _defaultParent;
         private CanvasGroup _canvasGroup;
+        private Transform _dragTransform;
+        private DraggableObject _dragObject;
+        public bool IsCopy { get; private set; } = false;
         
+        public DraggableObject DragObject
+        {
+            get => IsCreateCopy ? _dragObject : null;
+            private set => _dragObject = value; 
+        }
+
         private void Start()
         {
             _canvasGroup = gameObject.GetComponent<CanvasGroup>();
         }
-
-        public void OnDrag(PointerEventData eventData)
-        {
-            transform.position = Input.mousePosition;
-        }
-
-        public void OnEndDrag(PointerEventData eventData)
-        {
-            if (transform.parent == _defaultParent)
-            {
-                transform.SetParent(_initialParent);
-                transform.localPosition = Vector3.zero;
-            }
-
-            if (_isTransparency)
-                ChangeTransparency();
-            _canvasGroup.blocksRaycasts = true;
-        }
-
-        public void OnBeginDrag(PointerEventData eventData)
+        
+        private Transform InitializeTransform()
         {
             _initialParent = transform.parent;
             _defaultParent = _initialParent.parent.parent;
-            transform.SetParent(_defaultParent);
+            if (!IsCreateCopy || IsCopy)
+                return transform;
+            DragObject = Instantiate(this, transform.position, transform.rotation, _defaultParent);
+            DragObject.IsCopy = true;
+            var copyCard = DragObject.GetComponent<CharacterCard>();
+            copyCard.InitializeCard(GetComponent<CharacterCard>().Character, false);
+            _canvasGroup = DragObject.GetComponent<CanvasGroup>();
+            return DragObject.transform;
+        }
+        
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            _dragTransform = InitializeTransform();
+            _dragTransform.SetParent(_defaultParent);
             if(_isTransparency)
                 ChangeTransparency(_aplha);
             _canvasGroup.blocksRaycasts = false;
         }
 
+        public void OnDrag(PointerEventData eventData)
+        {
+            _dragTransform.position = Input.mousePosition;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            if (_dragTransform.parent == _defaultParent)
+            {
+                if(IsCreateCopy && IsCopy)
+                    Destroy(gameObject);
+                else if (IsCreateCopy)
+                    Destroy(DragObject.gameObject);
+                else
+                {
+                    _dragTransform.SetParent(_initialParent);
+                    _dragTransform.localPosition = Vector3.zero;    
+                }
+            }
+            if (_isTransparency)
+                ChangeTransparency();
+            _canvasGroup.blocksRaycasts = true;
+        }
+
         private void ChangeTransparency(float alpha = 1)
         {
             if(_image is null)
-                Debug.Log("Image is null in DraggableObject");
+                Logging.LogException(new NullReferenceException("Image is null in DraggableObject"));
             var color = _image.color;
             color.a = alpha;
             _image.color = color;
--- Assets/Scripts/RoguelikeMap/UI/Characters/DropZone.cs
+++ Assets/Scripts/RoguelikeMap/UI/Characters/DropZone.cs
@@ -1,5 +1,7 @@
 using System;
+using JetBrains.Annotations;
 using StartSessionMenu.ChooseCharacter.CharacterCard;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -7,20 +9,43 @@ namespace RoguelikeMap.UI.Characters
 {
     public class DropZone : MonoBehaviour, IDropHandler
     {
+        [CanBeNull] public CharacterCard CharacterCard { get; private set; }
+        public bool IsEmpty => CharacterCard is null;
         public event Action<DropZone, CharacterCard> OnTrySelect;
-        public bool IsSelected { get; private set; } = false;
-        
+
         public void OnDrop(PointerEventData eventData)
         {
-            eventData.pointerDrag.gameObject.TryGetComponent<CharacterCard>(out var characterCard);
+            if (!eventData.pointerDrag.gameObject.TryGetComponent<CharacterCard>(out var characterCard))
+                return;
+            if (characterCard.TryGetComponent<DraggableObject>(out var draggableObject))
+                if(draggableObject.IsCreateCopy && !draggableObject.IsCopy)
+                    characterCard = draggableObject.DragObject.GetComponent<CharacterCard>();
             if (characterCard is null)
                 return;
             OnTrySelect?.Invoke(this, characterCard);
         }
 
-        public void Select()
+        public void Select(CharacterCard card)
         {
-            IsSelected = !IsSelected;
+            if(CharacterCard is not null)
+                Unselect();
+            CharacterCard = card;
+        }
+
+        private void Unselect()
+        {
+            if(!CharacterCard.IsDestroyed())
+                Destroy(CharacterCard.gameObject);
+            CharacterCard = null;
+        }
+
+        public int TryGetCost()
+        {
+            if (IsEmpty)
+                return -1;
+            if (CharacterCard is not CharacterCardWithCost characterCardWithCost)
+                return -1;
+            return characterCardWithCost.Cost;
         }
     }
 }
