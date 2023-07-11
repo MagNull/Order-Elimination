fc9e6b53331673cdb00154848e25f1bbe0251414
MagNull <pathfo@yandex.ru>
2023-07-11 19:35:53 +0500

    Fix heals panel bug

--- Assets/AbilitySystem/PrototypeHelpers/BattleEndHandler.cs
+++ Assets/AbilitySystem/PrototypeHelpers/BattleEndHandler.cs
@@ -122,7 +122,7 @@ public class BattleEndHandler : MonoBehaviour
     {
         await OnBattleEnded();
         //_textEmitter.Emit($"������ �����.", Color.green, new Vector3(0, 1, -1), Vector3.zero, 1.2f, 100, fontSize: 2f);
-        var playerCharacters = _mediator.Get<GameCharacter[]>("player characters").ToArray();
+        var playerCharacters = _scenesMediator.Get<IEnumerable<GameCharacter>>("player characters").ToArray();
         var panel = (BattleVictoryPanel)UIController.SceneInstance.OpenPanel(PanelType.BattleVictory);
         var battleResult = CalculateBattleResult(BattleOutcome.Win);
         panel.UpdateBattleResult(
@@ -131,7 +131,7 @@ public class BattleEndHandler : MonoBehaviour
             battleResult.ItemsReward,
             () => SceneManager.LoadSceneAsync(OnExitSceneId));
         Logging.Log($"Current squad [{playerCharacters.Length}]: {string.Join(", ", playerCharacters.Select(c => c.CharacterData.Name))}" % Colorize.Red);
-        _mediator.Register("player characters", BattleUnloader.UnloadCharacters(_battleContext, playerCharacters));
+        _scenesMediator.Register("player characters", BattleUnloader.UnloadCharacters(_battleContext, playerCharacters));
         _scenesMediator.Register("battle results", battleResult);
     }
 
@@ -142,7 +142,7 @@ public class BattleEndHandler : MonoBehaviour
         var panel = (BattleDefeatPanel)UIController.SceneInstance.OpenPanel(PanelType.BattleDefeat);
         var battleResult = CalculateBattleResult(BattleOutcome.Lose);
         panel.UpdateBattleResult(
-            _mediator.Get<GameCharacter[]>("player characters"), 
+            _scenesMediator.Get<IEnumerable<GameCharacter>>("player characters"), 
             battleResult.MoneyReward, 
             () => SceneManager.LoadSceneAsync(OnRetrySceneId),
             () => SceneManager.LoadSceneAsync(OnExitSceneId));
