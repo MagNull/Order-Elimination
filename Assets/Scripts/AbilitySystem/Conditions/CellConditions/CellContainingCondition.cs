f9242c88d52a7946513066ba4b064a83cdfd9b1a
Fakumen <denis11180@gmail.com>
2023-09-12 12:27:08 +0500

    More Optimization
    - Pattern usage in conditions optimized

--- Assets/Scripts/AbilitySystem/Conditions/CellConditions/CellContainingCondition.cs
+++ Assets/Scripts/AbilitySystem/Conditions/CellConditions/CellContainingCondition.cs
@@ -41,28 +41,22 @@ namespace OrderElimination.AbilitySystem
 
         public bool IsConditionMet(IBattleContext battleContext, AbilitySystemActor askingEntity, Vector2Int positionToCheck)
         {
-            AbilitySystemActor[] cellEntities;
-            if (VisibleEntitiesOnly)
-                cellEntities = battleContext.GetVisibleEntitiesAt(positionToCheck, askingEntity.BattleSide).ToArray();
-            else
-                cellEntities = battleContext.BattleMap.GetContainedEntities(positionToCheck).ToArray();
+            var cellEntities = VisibleEntitiesOnly
+                ? battleContext.GetVisibleEntitiesAt(positionToCheck, askingEntity.BattleSide).ToArray()
+                : battleContext.BattleMap.GetContainedEntities(positionToCheck).ToArray();
             var isCellEmpty = cellEntities.Length == 0;
-            if (MustBeEmpty) 
-                return isCellEmpty;
+            if (isCellEmpty) 
+                return MustBeEmpty || AllowEmptyCells;
             if (AllEntitiesMustMeetRequirements)
             {
-                return AllowEmptyCells && isCellEmpty
-                    || cellEntities.All(e => EntityConditions.All(c => c.IsConditionMet(battleContext, askingEntity, e)));
+                return cellEntities.All(e => EntityConditions.All(c => c.IsConditionMet(battleContext, askingEntity, e)));
             }
             else
             {
-                if (cellEntities == null)
-                    throw new InvalidProgramException();
                 var view = battleContext.EntitiesBank.GetViewByEntity(askingEntity);
                 if (EntityConditions == null)
-                    Debug.LogError($"{nameof(EntityConditions)} on {view.Name}");
-                return AllowEmptyCells && isCellEmpty
-                    || cellEntities.Any(e => EntityConditions.All(c => c.IsConditionMet(battleContext, askingEntity, e)));
+                    Debug.LogError($"{nameof(EntityConditions)} null on {view.Name}");
+                return cellEntities.Any(e => EntityConditions.All(c => c.IsConditionMet(battleContext, askingEntity, e)));
             }
         }
     }
---
+++ Assets/Scripts/AbilitySystem/Conditions/ICellCondition.cs
@@ -0,0 +1,21 @@
+﻿using OrderElimination.Infrastructure;
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace OrderElimination.AbilitySystem
+{
+    [GUIColor(1, 1, 0)]
+    public interface ICellCondition : ICloneable<ICellCondition>
+    {
+        public bool IsConditionMet(IBattleContext battleContext, AbilitySystemActor askingEntity, Vector2Int positionToCheck);
+
+        //For cluster-check optimizations
+        public virtual Vector2Int[] FilterMany(IBattleContext battleContext, AbilitySystemActor askingEntity, IEnumerable<Vector2Int> positions)
+        {
+            return positions.Where(p => IsConditionMet(battleContext, askingEntity, p)).ToArray();
+        }
+    }
+}
