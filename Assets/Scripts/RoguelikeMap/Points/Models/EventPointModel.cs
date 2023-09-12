0b4dda1d830c2b4ee3f4134aeefe9681b9a44ff6
Tupi8mpl <i.tupikov12@gmail.com>
2023-09-12 22:58:10 +0500

    Add Scenario field to BattleNode

--- Assets/Scripts/RoguelikeMap/Points/Models/EventPointModel.cs
+++ Assets/Scripts/RoguelikeMap/Points/Models/EventPointModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Events;
-using OrderElimination.MacroGame;
 using RoguelikeMap.SquadInfo;
 using RoguelikeMap.UI.PointPanels;
 using UnityEngine;
@@ -16,13 +15,10 @@ namespace RoguelikeMap.Points.Models
         
         [SerializeReference]
         private EventPointGraph _eventGraph;
-        [SerializeField]
-        private BattleScenario _battleScenario;
         
         protected EventPanel Panel => _panel as EventPanel;
         
         public override PointType Type => PointType.Event;
-        public BattleScenario Scenario => _battleScenario;
         
         public override async Task Visit(Squad squad)
         {
