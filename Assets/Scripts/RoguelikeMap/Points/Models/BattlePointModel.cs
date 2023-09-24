984fe301497bac8971638cad26d6a50655e7720a
love_coding <i.tupikov12@gmail.com>
2023-09-24 20:49:25 +0500

    Fix save point for switch scene, active members, sceneMediator Get

--- Assets/Scripts/RoguelikeMap/Points/Models/BattlePointModel.cs
+++ Assets/Scripts/RoguelikeMap/Points/Models/BattlePointModel.cs
@@ -1,11 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameInventory.Items;
+using OrderElimination;
+using OrderElimination.MacroGame;
+using RoguelikeMap.SquadInfo;
+using RoguelikeMap.UI.PointPanels;
+using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace RoguelikeMap.Points.Models
 {
     [Serializable]
-    public class BattlePointModel : FinalBattlePointModel
+    public class BattlePointModel : PointModel
     {
+        [Input] public PointModel entries;
         [Output] public PointModel exits;
+        
+        [SerializeField]
+        private List<CharacterTemplate> _enemies;
+        [SerializeField]
+        private BattleScenario _battleScenario;
+
+        [SerializeField]
+        private SerializedDictionary<ItemData, float> _itemsDropProbability;
+
+        private List<GameCharacter> _enemiesGameCharacter;
+
+        protected BattlePanel Panel => panel as BattlePanel;
+
         public override PointType Type => PointType.Battle;
+        public IReadOnlyList<IGameCharacterTemplate> Enemies => _enemies;
+        public BattleScenario Scenario => _battleScenario;
+        public Dictionary<ItemData, float> ItemsDropProbability => _itemsDropProbability;
+
+        public override void ShowPreview(Squad squad)
+        {
+            squad.OnUpdateMembers -= Panel.UpdateAlliesOnMap;
+            squad.OnUpdateMembers += Panel.UpdateAlliesOnMap;
+            _enemiesGameCharacter = GameCharactersFactory.CreateGameCharacters(Enemies).ToList();
+            Panel.Initialize(_battleScenario, _enemiesGameCharacter, squad.Members, Index); //TODO: Store GameCharacters
+            if(!Panel.IsOpen)
+                Panel.Open();
+        }
+
+        public override async Task Visit(Squad squad)
+        {
+            await squad.Visit(this);
+        }
     }
 }
--- Assets/Scripts/RoguelikeMap/Points/Models/FinalBattlePointModel.cs
+++ Assets/Scripts/RoguelikeMap/Points/Models/FinalBattlePointModel.cs
@@ -12,44 +12,8 @@ using UnityEngine.Rendering;
 
 namespace RoguelikeMap.Points.Models
 {
-    public class FinalBattlePointModel : PointModel
+    public class FinalBattlePointModel : BattlePointModel
     {
-        [Input] public PointModel entries;
-        
-        [SerializeField]
-        private List<CharacterTemplate> _enemies;
-        [SerializeField]
-        private BattleScenario _battleScenario;
-
-        [SerializeField]
-        private SerializedDictionary<ItemData, float> _itemsDropProbability;
-
-        private List<GameCharacter> _enemiesGameCharacter;
-        private Squad _squad;
-
-        protected BattlePanel Panel => panel as BattlePanel;
-
         public override PointType Type => PointType.Battle;
-        public IReadOnlyList<IGameCharacterTemplate> Enemies => _enemies;
-        public BattleScenario Scenario => _battleScenario;
-
-        private Func<Task> _myTask;
-        public Dictionary<ItemData, float> ItemsDropProbability => _itemsDropProbability;
-
-        public override void ShowPreview(Squad squad)
-        {
-            _squad = squad;
-            squad.OnUpdateMembers -= Panel.UpdateAlliesOnMap;
-            squad.OnUpdateMembers += Panel.UpdateAlliesOnMap;
-            _enemiesGameCharacter = GameCharactersFactory.CreateGameCharacters(Enemies).ToList();
-            Panel.Initialize(_battleScenario, _enemiesGameCharacter, squad.Members, Index); //TODO: Store GameCharacters
-            if(!Panel.IsOpen)
-                Panel.Open();
-        }
-
-        public override async Task Visit(Squad squad)
-        {
-            await squad.Visit(this);
-        }
     }
 }
