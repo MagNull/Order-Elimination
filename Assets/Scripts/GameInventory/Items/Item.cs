60ad06a6e51cb1cd7d9e470101c086bb34b73fbc
MagNull <pathfo@yandex.ru>
2023-09-27 21:04:46 +0500

    Update heal panel

--- Assets/Scripts/GameInventory/Items/Item.cs
+++ Assets/Scripts/GameInventory/Items/Item.cs
@@ -28,64 +28,6 @@ namespace GameInventory.Items
         }
     }
 
-    [Serializable]
-    public class EquipmentItem : Item
-    {
-        [SerializeField]
-        private PassiveAbilityBuilder _equipAbility;
-
-        public EquipmentItem(ItemData itemData) : base(itemData)
-        {
-            _equipAbility = itemData.EquipAbility;
-        }
-
-        public override void OnTook(AbilitySystemActor abilitySystemActor)
-        {
-            abilitySystemActor.GrantPassiveAbility(
-                new PassiveAbilityRunner(AbilityFactory.CreatePassiveAbility(_equipAbility),
-                    AbilityProvider.Equipment));
-        }
-    }
-
-    [Serializable]
-    public class ConsumableItem : Item
-    {
-        public event Action<ConsumableItem> UseTimesOver;
-
-        [SerializeField]
-        private ActiveAbilityBuilder _useAbility;
-
-        [SerializeField]
-        private int _useTimes;
-
-        protected int UseTimes
-        {
-            get => _useTimes;
-            set
-            {
-                _useTimes = value;
-                if (_useTimes > 0)
-                    return;
-                UseTimesOver?.Invoke(this);
-            }
-        }
-
-        public ConsumableItem(ItemData itemData) : base(itemData)
-        {
-            _useAbility = itemData.UseAbility;
-            _useTimes = itemData.UseTimes;
-        }
-
-        public override void OnTook(AbilitySystemActor abilitySystemActor)
-        {
-            var ability = new ActiveAbilityRunner(AbilityFactory.CreateActiveAbility(_useAbility),
-                AbilityProvider.Equipment);
-            abilitySystemActor.GrantActiveAbility(ability);
-            UseTimesOver += _ => abilitySystemActor.RemoveActiveAbility(ability);
-            ability.AbilityExecutionStarted += _ => UseTimes--;
-        }
-    }
-
     public interface IUsable
     {
         public void Use(GameCharacter gameCharacter);
